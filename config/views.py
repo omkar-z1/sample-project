25a72a6ada82c7f6b3eb6920e52f1e1f5474cafb
omkar-z1 <omkar.z1@sankeysolutions.com>
2026-02-13 17:33:03 +0530

    Update home response message to reflect recent changes after Kubernetes deployment

--- config/views.py
+++ config/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello from Kubernetes 🚀")
+    return HttpResponse("Hello from Kubernetes! changes done after k8s deploy 🚀")
